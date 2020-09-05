f64b8f3ceb527dbbdf0f4e664ffd86c3e3a42e43
aakawazu <aakawazu@protonmail.com>
2020-09-06 04:15:18 +0900

    Update footer

--- components/footer.js
+++ components/footer.js
@@ -3,14 +3,7 @@ import styles from '../styles/Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-      </a>
+      <a>©第98回武蔵高等学校中学校記念祭小委員会</a>
     </footer>
   )
 }
